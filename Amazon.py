f54e37d4e1906d0160cae66f9574b5a312c29b71
sanaica <sanaica.dsouza@gmail.com>
2025-03-24 00:38:07 +0530

    Testing 2 also successful

--- Amazon.py
+++ Amazon.py
@@ -74,4 +74,3 @@ if __name__ == "__main__":
     product_name = input("Enter product name: ")
     num_products = int(input("Enter which number product you want "))
     search_amazon(product_name, num_products)
-print("Testing error again")
